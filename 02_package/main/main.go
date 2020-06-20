6f3afcdea8d94f834fe6859e2000217d78222551
kshimauchi <kshimauchi@gmail.com>
2020-06-19 17:21:20 -0700

    package

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
